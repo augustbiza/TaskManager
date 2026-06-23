c8f07c6969a3f1e05c5716cfb80a97558361bf19
Augusto Bizarria <augustogomesb94@gmail.com>
2026-06-23 20:55:25 -0300

    feat: add initial CLI

--- src/main/java/com/taskmanager/Main.java
+++ src/main/java/com/taskmanager/Main.java
@@ -2,10 +2,81 @@ package com.taskmanager;
 
 import com.taskmanager.service.TaskService;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
+        
         TaskService service = new TaskService();
 
-        service.toggleTask(7);
+        String option;
+        System.out.println("===== TASK MANAGER =====");
+        
+        do {
+            System.out.println("Choose your option:\n1-List Tasks\n2-Create Task\n3-Update Task\n4-Delete Task\n0-Exit");
+            option = scan.nextLine();
+            long id;
+            String title;
+            switch(option) {
+                case "0" :
+                    System.out.println("========================");
+                    break;
+                case "1":
+                    System.out.println("Tasks: ");
+                    service.getAllTasks().forEach(System.out::println);
+                    System.out.println();
+                    
+                    break;
+                case "2":
+                    System.out.print("Title: ");
+                    title = scan.nextLine();
+
+                    service.createTask(title);
+                    System.out.println("Task created!\n");
+
+                    break;
+                case "3":
+                    service.getAllTasks().forEach(System.out::println);
+
+                    System.out.print("Task ID: ");
+                    id = scan.nextLong(); scan.nextLine();
+
+                    System.out.print("New title: ");
+                    String newTitle = scan.nextLine();
+
+                    System.out.print("Completed (true/false): ");
+                    boolean completed = scan.nextBoolean(); scan.nextLine();
+
+                    service.updateTask(id, newTitle, completed);
+                    System.out.println("Task updated!\n");
+
+                    break;
+                case "4":
+                    service.getAllTasks().forEach(System.out::println);
+
+                    System.out.print("Task ID: ");
+                    id = scan.nextLong(); scan.nextLine();
+
+                    service.deleteTask(id);
+                    System.out.println("Task deleted!\n");
+
+                    break;
+                case "5":
+                    service.getAllTasks().forEach(System.out::println);
+
+                    System.out.println("Task ID: ");
+                    id = scan.nextLong(); scan.nextLine();
+
+                    service.toggleTask(id);
+                    System.out.println("Task updated!\n");
+
+                    break;
+                default:
+                    System.out.println("Invalid Option\n");
+                    break;
+            }
+        } while(!option.equals("0"));
     }
 }
--- src/main/java/com/taskmanager/service/TaskService.java
+++ src/main/java/com/taskmanager/service/TaskService.java
@@ -13,9 +13,8 @@ public class TaskService {
     }
 
     public void createTask(String title) {
-        if(title == null || title.isBlank()) {
-            throw new IllegalArgumentException("Task can not be empty!");
-        }
+        
+        validadeTitle(title);
 
         Task task = new Task(title);
         taskDAO.create(task);
@@ -26,12 +25,42 @@ public class TaskService {
     }
 
     public void deleteTask(long id) {
-        if(id < 1) throw new IllegalArgumentException("Invalid \"ID\"");
+        
+        validateId(id);
     
         taskDAO.delete(id);
     }
 
+    public void updateTask(long id, String newTitle, boolean completed) {
+        
+        validateId(id);
+        validadeTitle(newTitle);
+
+        Task currentTask = taskDAO.findById(id);
+        if(currentTask == null) {
+            throw new IllegalArgumentException("Task not found");
+        }
+
+        Task updatedTask = new Task(id, newTitle, completed);
+        taskDAO.update(updatedTask);
+    }
+
     public void toggleTask(long id) {
         taskDAO.toggleCompleted(id);
+    }
+
+
+    private void validadeTitle(String title) {
+        if(title == null || title.isBlank()) {
+            throw new IllegalArgumentException("Task title can not be empty!");
+        }
+
+        if(title.length() > 100) {
+            throw new IllegalArgumentException("Title can not be that long!");
+        }
+    }
+
+    private void validateId(long id) {
+        if(id < 1) throw new IllegalArgumentException("Invalid \"ID\"");
     }
 }
